6909105602e75f2a62a2cb10157a5662081c1ed7
Moussa <Moussa@M>
2022-09-03 22:26:26 +0200

    første oppgave

---
+++ 0,1/src/oppbevaring.java
@@ -0,0 +1,9 @@
+
+public class oppbevaring {
+
+	public static void main(String[] args) {
+	 System.out.println("hello world ");
+	 System.out.println("opppgave 1");
+	}
+
+}
---
+++ Oppgaver/src/Oppgave1.java
@@ -0,0 +1,10 @@
+
+public class Oppgave1 {
+
+	public static void main(String[] args) {
+		
+int x=5;
+int y=7;
+	}
+
+}
